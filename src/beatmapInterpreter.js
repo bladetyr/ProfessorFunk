fa1ef03cfd6a057f90f4546ed816ce63de41a2eb
bladetyr <bladetyr@udel.edu>
2023-09-21 11:04:55 -0400

    skeleton for note handling

--- src/beatmapInterpreter.js
+++ src/beatmapInterpreter.js
@@ -18,3 +18,25 @@ function playAudio(beatMap){
     return 0;
 }
 
+// HANDLE NOTES
+function spawnNote(direction){
+    //dictates where on screen note spawns based on direction
+    if(direction == 'LEFT'){
+        //pass
+    }else if(direction == 'RIGHT'){
+        //pass
+    }else if(direction == 'UP'){
+        //pass
+    }else{
+        //pass
+    }
+}
+
+function scrollNote(note, scrollTime){
+    //takes in note somehow? maybe grab by element ID?
+    //can we generate unique note IDs and then pass them in here?
+
+    //insert code and math to raise spawned note along Y axis over the course of X seconds (scrollTime provided by beatmap.txt file)
+
+}
+
