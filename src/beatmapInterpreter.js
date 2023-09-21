26df613abdb71037e85be199f18bf506ceb9e3d6
bladetyr <bladetyr@udel.edu>
2023-09-21 08:26:07 -0400

    beatmap parser works

--- src/beatmapInterpreter.js
+++ src/beatmapInterpreter.js
@@ -1,15 +1,15 @@
 var jQueryScript = document.createElement('script');  
-jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js');
 document.head.appendChild(jQueryScript);
 
-//DIRECTION/TIME TAKEN TO GO UP/TIME UNTIL READING NEXT NOTE
+// DIRECTION/TIME TAKEN TO GO UP/TIME UNTIL READING NEXT NOTE
 var file = "../beatmaps/exampleBeatmap.txt";
 function parseBeatmap(){
     $.get(file,function(txt){
-        var lines = txt.responseText.split("\n");
+        var lines = txt.split("\n");
         for (var i = 0, len = lines.length; i < len; i++) {
-            save(lines[i]);
             console.log(lines[i]);
         }
     }); 
+    return 0;
 }
