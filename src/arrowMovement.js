1470fb8dc105b11ba24e690df6a5659896eec60f
bladetyr <bladetyr@udel.edu>
2023-11-27 14:15:07 -0500

    arrows move, tests includes still, arrows removed from index.html

--- src/arrowMovement.js
+++ src/arrowMovement.js
@@ -1,77 +1,19 @@
-/*
+// TEMP VARIABLES
+var dir = "up";
+var speed = 2;
+var wait = 1;
 
-let image = document.querySelector('img');
-let moveBy = 10;
+// SPAWN NOTES
+function spawnNote(direction){
+    var arrow = document.createElement()
+    if(direction == "up"){
 
-window.addEventListener('keydown', e => {
-    const top = image.style.top ? image.style.top : 0
-    const left = image.style.left ? image.style.left : 0
-    switch(e.key) {
-        case 'ArrowLeft':
-            image.style.left = (parseInt(left) - moveBy) + 'px';
-            break;
-        case 'ArrowRight': 
-            image.style.left = (parseInt(left) + moveBy) + 'px';
-            break;
-        case 'ArrowUp':
-            image.style.top = (parseInt(top) - moveBy) + 'px';
-            break;
-        case 'ArrowDown':
-            image.style.top = (parseInt(top) + moveBy ) + 'px';
-            break;
+    }else if(direction == "down"){
+
+    }else if(direction == "left"){
+
+    }else{
 
     }
-})
-
-*/
-
-/*const board = document.getElementById('board');
-const generator = document.getElementById('new-line');
-
-const createRow = ()=>{
-
-    const newRow = board.cloneNode(true);
-
-generator.append(newRow);
-
 }
-
-*/
-
-//making it so it reads index.html?
-
-function loadHTML(){
-    fetch('index.html')
-}
-
-/* 
-    javascript timing to make it dissapear, have to make it so 
-    that it goes up and matches the corresponding arrow later 
-*/
-
-function changeArrows() {
-    var canvas = document.getElementById("arrows");
-    canvas.src = arrows[x];
-    x++;
-    if(x>=arrows.length){
-        x = 0;
-    }
-    fadeCanvas(canvas, 100, true);
-     setTimeout("changeArrows()", 3000);
-}
-
-function fadeCanvas(i, e, fade) {
-    if(fade === true ) {
-        e--;
-    } else {
-        e++;
-    }
-    if(e > 0 && e <100) {
-        i.style.opacity = e / 100;
-        setTimeout(function() { fadeCanvas(i, e, fade); }, 10);
-    }
-}
-
-var arrows = [], x = 0;
-arrows[0] = "images/leftArrow.png";
-setTimeout("changeArrows()", 3000);
+// MOVE NOTES
--- src/beatmapInterpreter.js
+++ src/beatmapInterpreter.js
@@ -1,13 +1,17 @@
 // DIRECTION/TIME TAKEN TO GO UP/TIME UNTIL READING NEXT NOTE
-var file = "../beatmaps/";
 function parseBeatmap(beatMap){
+    spawnNote("DOWN");
+    spawnNote("UP");
+    spawnNote("LEFT");
+    spawnNote("RIGHT");
+    var file = "../beatmaps/"; 
     file += beatMap;
     $.get(file,function(txt){
         var lines = txt.split("\n");
         for (var i = 0, len = lines.length; i < len; i++) {
             console.log(lines[i]);
         }
-    }); 
+    });
     return 0;
 }
 
@@ -18,25 +22,41 @@ function playAudio(beatMap){
     return 0;
 }
 
-// HANDLE NOTES
+// TEMP VARIABLES FOR ARROW MOVEMENT TESTING
+var dir = "LEFT";
+var speed = 2;
+var wait = 1;
+
+// SPAWN NOTES
 function spawnNote(direction){
+    console.log("here");
+    var arrow = document.createElement("img");
     //dictates where on screen note spawns based on direction
     if(direction == 'LEFT'){
-        //pass
+        arrow.src = "images/leftArrow.png";
+        arrow.setAttribute("style","margin-left:-28%;bottom:-120px;position:absolute");
     }else if(direction == 'RIGHT'){
-        //pass
+        arrow.src = "images/rightArrow.png";
+        arrow.setAttribute("style","margin-left:14%;bottom:-120px;position:absolute");
     }else if(direction == 'UP'){
-        //pass
+        arrow.src = "images/upArrow.png";
+        arrow.setAttribute("style","margin-left:0%;bottom:-120px;position:absolute");
     }else{
-        //pass
+        arrow.src = "images/downArrow.png";
+        arrow.setAttribute("style","margin-left:-14%;bottom:-120px;position:absolute");
     }
+    var appendThis = document.getElementById("arrows");
+    appendThis.appendChild(arrow);
+    //make note scroll
+    scrollNote(arrow, speed);
 }
 
 function scrollNote(note, scrollTime){
+    //inspired by https://stackoverflow.com/questions/12712592/how-to-make-a-small-image-move-from-one-side-of-the-screen-to-the-other-with-js
     //takes in note somehow? maybe grab by element ID?
     //can we generate unique note IDs and then pass them in here?
 
-    //insert code and math to raise spawned note along Y axis over the course of X seconds (scrollTime provided by beatmap.txt file)
-
+    //screen height / scrollTime, for for loop, clear and redraw image (hide and show?)
+    note.animate({bottom: "800px"}, scrollTime*1000, scrollNote);
 }
 
